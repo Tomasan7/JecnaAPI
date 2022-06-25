2d998ed731acf8aaacfe70a268df78efb34e23ce
Tomasan7 <tomashula06@gmail.com>
2022-06-25 12:40:12 +0200

    Comment Behaviour class

--- src/main/kotlin/me/tomasan7/jecnaapi/data/grade/Behaviour.kt
+++ src/main/kotlin/me/tomasan7/jecnaapi/data/grade/Behaviour.kt
@@ -1,14 +1,24 @@
 package me.tomasan7.jecnaapi.data.grade
 
-data class Behaviour(val notifications: List<Notification>, val finalGrade: FinalGrade)
+/**
+ * Represents the behaviour row in the grades table.
+ */
+data class Behaviour(val notifications: List<Notification>,
+                     val finalGrade: FinalGrade)
 {
     companion object
     {
         const val SUBJECT_NAME = "Chování"
     }
 
+    /**
+     * Represents single notification in the row about the student's behaviour.
+     */
     data class Notification(val type: NotificationType, val message: String)
 
+    /**
+     * Type of the [Notification], either [GOOD] or [BAD].
+     */
     enum class NotificationType
     {
         GOOD,
