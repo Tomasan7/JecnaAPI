01a9b0d36ef3fab7aace72b81e12e647d9b7215f
Tomasan7 <tomashula06@gmail.com>
2022-06-26 18:16:19 +0200

    Clean up HtmlAttendancesPageParserImpl

--- src/main/kotlin/me/tomasan7/jecnaapi/parser/parsers/HtmlAttendancesPageParserImpl.kt
+++ src/main/kotlin/me/tomasan7/jecnaapi/parser/parsers/HtmlAttendancesPageParserImpl.kt
@@ -3,8 +3,12 @@ package me.tomasan7.jecnaapi.parser.parsers
 import me.tomasan7.jecnaapi.data.Attendance
 import me.tomasan7.jecnaapi.data.AttendancesPage
 import me.tomasan7.jecnaapi.parser.ParseException
+import me.tomasan7.jecnaapi.util.emptyMutableLinkedList
+import me.tomasan7.jecnaapi.util.month
+import me.tomasan7.jecnaapi.util.toSchoolYear
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
 import java.time.LocalDate
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
@@ -23,40 +27,22 @@ class HtmlAttendancesPageParserImpl : HtmlAttendancesPageParser
             val attendancesPageBuilder = AttendancesPage.builder()
 
             val document = Jsoup.parse(html)
+
             /* All the rows (tr) in the absence table. */
-            val rowEles = document.select(".tab.absence-list > tbody > tr")
+            val rowEles = document.select(".absence-list > tbody > tr")
 
             for (rowEle in rowEles)
             {
                 /* The first column in the row, which contains the day date. */
                 val dayEle = rowEle.selectFirst(".date")
                 val day = parseDayDate(dayEle!!.text(), document)
-                /* The second column in the row, which contains all the paragraphs with the texts. */
-                val attendancesEle = rowEle.select("td")[1]
-                val attendancesParagraphs = attendancesEle.select("p")
-                /* Contains all entries/leaves in the day as string. */
-                val dayAttendancesAsStr = mutableListOf<String>()
+                /* The second column in the row, which contains all the attendances. */
+                val mainColumnEle = rowEle.select("td")[1]
 
-                for (attendanceParagraph in attendancesParagraphs)
-                {
-                    val dayAttendances = attendanceParagraph.text().split(", ")
-                    dayAttendancesAsStr.addAll(dayAttendances)
-                }
+                val attendanceList = parseAttendances(mainColumnEle)
 
-
-                for (dayAttendanceStr in dayAttendancesAsStr)
-                {
-                    /* Don't save the days, which have no presence. */
-                    if (dayAttendanceStr.isBlank())
-                        continue
-
-                    /* If the attendance matches regex, then it is an exit. */
-                    val exit = LEAVE_REGEX.containsMatchIn(dayAttendanceStr)
-                    /* Find the time and parse it to the LocalTime object. */
-                    val time = LocalTime.parse(TIME_REGEX.find(dayAttendanceStr)!!.value, DateTimeFormatter.ofPattern("H:mm"))
-
-                    attendancesPageBuilder.addAttendance(day, Attendance(exit, time))
-                }
+                if (attendanceList.isNotEmpty())
+                    attendancesPageBuilder.setAttendances(day, attendanceList)
             }
 
             return attendancesPageBuilder.build()
@@ -68,41 +54,82 @@ class HtmlAttendancesPageParserImpl : HtmlAttendancesPageParser
     }
 
     /**
+     * Parses all [attendances][Attendance] from the main column.
+     *
+     * @return [List] of [Attendance].
+     */
+    private fun parseAttendances(attendancesColumnEle: Element): List<Attendance>
+    {
+        val attendanceList = emptyMutableLinkedList<Attendance>()
+
+        /* All the lines with the attendances. (as of now, two attendances per line) */
+        val lines = attendancesColumnEle.select("p")
+
+        /* Contains all entries/leaves in the day as string. */
+        val dayAttendancesAsStr = mutableListOf<String>()
+
+        for (attendanceParagraph in lines)
+            dayAttendancesAsStr.addAll(attendanceParagraph.text().split(", "))
+
+        for (dayAttendanceStr in dayAttendancesAsStr)
+        {
+            /* If the attendance matches regex, then it is an exit. */
+            val exit = LEAVE_REGEX.containsMatchIn(dayAttendanceStr)
+            /* Find the time and parse it to the LocalTime object. */
+            val time = LocalTime.parse(TIME_REGEX.find(dayAttendanceStr)!!.value, TIME_FORMATTER)
+
+            attendanceList.add(Attendance(exit, time))
+        }
+
+        return attendanceList
+    }
+
+    /**
      * Parses a date from the string and then uses the data from a different part of the page to determine the year.
-     * @param dayEleText The text to parse the date from.
+     * @param dayStr The text to parse the date from.
      * @param document The document, because the method needs to find the year on the page.
      * @return The parsed [LocalDate] object.
      */
-    private fun parseDayDate(dayEleText: String, document: Document): LocalDate
+    private fun parseDayDate(dayStr: String, document: Document): LocalDate
     {
-        val date = DATE_REGEX.find(dayEleText)!!.value
+        val date = DATE_REGEX.find(dayStr)!!.value
         /* First index with the day and the second with the month. */
-        val dateSplit = date.split(".")
-        val monthOfYear = dateSplit[1].toByte()
+        val monthOfYear = date.split(".")[1].toInt()
 
-        /* First index with the first year and the second with the second. (eg. 2021/2022) */
-        val schoolYear = document.selectFirst("#schoolYearId > option")!!.text().split("/")
-        val firstYear = schoolYear[0].toShort()
-        val secondYear = schoolYear[1].toShort()
-
-        /* If the month is earlier than or same as August, use the second calendar year, the first otherwise.  */
-        val resultYear = if (monthOfYear <= 8) secondYear else firstYear
+        /* Finds the current calendar year from the year dropdown selection. */
+        val schoolYear = document.selectFirst("#schoolYearId > option")!!.text().toSchoolYear()
+        val resultYear = schoolYear.getCalendarYear(monthOfYear.month()).toLong()
 
         return LocalDate.parse(date, DateTimeFormatterBuilder()
             .appendPattern("d.M.")
-            .parseDefaulting(ChronoField.YEAR, resultYear.toLong())
+            .parseDefaulting(ChronoField.YEAR, resultYear)
             .toFormatter())
     }
 
     companion object
     {
-        /* Matches a date in 'dd.MM.' format. (for speed and simplicity, it also matches non-existing dates) */
+        /**
+         * Matches the day date in the first column.
+         *
+         * Matches a date in 'dd.MM.' format. (for speed and simplicity, it also matches non-existing dates)
+         */
         private val DATE_REGEX = Regex("""[0-3]?\d\.[0-1]?\d\.""", RegexOption.DOT_MATCHES_ALL)
 
-        /* Matches whole string if it contains "Odchod" => if it's a leave. */
-        private val LEAVE_REGEX = Regex(""".*Odchod.*""", RegexOption.DOT_MATCHES_ALL)
+        /**
+         *  Matches "Odchod" **only** => if it's a leave.
+         */
+        private val LEAVE_REGEX = Regex("""Odchod""")
 
-        /* Matches time in hh:mm format. */
+        /**
+         * Matches the time in each attendance.
+         *
+         * Matches time in hh:mm format.
+         */
         private val TIME_REGEX = Regex("""(?:[0-1]?\d|2[0-3]):[0-5]\d""", RegexOption.DOT_MATCHES_ALL)
+
+        /**
+         * Formats from/into `"H:mm"` format.
+         */
+        private val TIME_FORMATTER = DateTimeFormatter.ofPattern("H:mm")
     }
 }
