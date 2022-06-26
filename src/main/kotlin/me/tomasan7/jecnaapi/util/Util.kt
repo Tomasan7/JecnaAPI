9fb8f3e35810253897bd6818ef035fbfe54bc157
Tomasan7 <tomashula06@gmail.com>
2022-06-26 16:59:48 +0200

    Add Int.month() function to Util

--- src/main/kotlin/me/tomasan7/jecnaapi/util/Util.kt
+++ src/main/kotlin/me/tomasan7/jecnaapi/util/Util.kt
@@ -1,8 +1,15 @@
 package me.tomasan7.jecnaapi.util
 
+import java.time.Month
 import java.util.LinkedList
 
 /**
  * Creates a new empty mutable [LinkedList].
  */
 fun <T> emptyMutableLinkedList() = LinkedList<T>()
+
+/**
+ * @return [Month] corresponding to this number.
+ * @see [Month.of]
+ */
+fun Int.month() = Month.of(this)
