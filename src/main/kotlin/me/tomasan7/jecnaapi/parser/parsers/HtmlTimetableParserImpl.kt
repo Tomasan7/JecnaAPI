0b925abfe279c0497737eb0e3c115e2dbd799e67
Tomasan7 <tomashula06@gmail.com>
2022-06-27 20:10:16 +0200

    Clean up HtmlTimetableParser

--- src/main/kotlin/me/tomasan7/jecnaapi/parser/parsers/HtmlTimetableParserImpl.kt
+++ src/main/kotlin/me/tomasan7/jecnaapi/parser/parsers/HtmlTimetableParserImpl.kt
@@ -1,11 +1,11 @@
 package me.tomasan7.jecnaapi.parser.parsers
 
-import me.tomasan7.jecnaapi.data.Lesson
-import me.tomasan7.jecnaapi.data.LessonPeriod
-import me.tomasan7.jecnaapi.data.LessonSpot
-import me.tomasan7.jecnaapi.data.TimetablePage
+import me.tomasan7.jecnaapi.data.*
 import me.tomasan7.jecnaapi.parser.ParseException
+import me.tomasan7.jecnaapi.util.Name
+import me.tomasan7.jecnaapi.util.emptyMutableLinkedList
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 
 /**
  * Parses correct HTML to [TimetablePage] instance.
@@ -20,7 +20,7 @@ class HtmlTimetableParserImpl : HtmlTimetablePageParser
 
             val document = Jsoup.parse(html)
 
-            /* All the rows (tr) in the grades table. */
+            /* All the rows (tr) in the timetable table. */
             val rowEles = document.select("table.timetable > tbody > tr")
 
             /* The row (tr) containing all the LessonPeriods details.
@@ -33,9 +33,8 @@ class HtmlTimetableParserImpl : HtmlTimetablePageParser
             val lessonPeriodEles = lessonPeriodsEle.select("th.period")
 
             /* Add all the LessonPeriods to the timetable. */
-            lessonPeriodEles.forEach {
-                timetablePageBuilder.addLessonPeriod(LessonPeriod.fromString(it.selectFirst(".time")!!.text()))
-            }
+            for (lessonPeriodEle in lessonPeriodEles)
+                timetablePageBuilder.addLessonPeriod(parseLessonPeriod(lessonPeriodEle))
 
             /* Removes the row with the LessonPeriods, so it leaves all the subjects. */
             rowEles.removeAt(0)
@@ -48,38 +47,7 @@ class HtmlTimetableParserImpl : HtmlTimetablePageParser
                 val lessonSpotEles = rowEle.select("td")
 
                 for (lessonSpotEle in lessonSpotEles)
-                {
-                    /* Skip if the lesson spot is empty, this leaves the lesson variable to null -> indicating no lesson. */
-                    if (lessonSpotEle.hasClass("empty"))
-                    {
-                        timetablePageBuilder.addLesson(day, null)
-                        continue
-                    }
-
-                    /* All the lessons in the lesson spot. */
-                    val lessonEles = lessonSpotEle.getElementsByTag("div")
-
-                    val lessons = ArrayList<Lesson>()
-
-                    for (lessonEle in lessonEles)
-                    {
-                        var group = 0
-                        val groupEle = lessonEle.selectFirst(".group")
-                        if (groupEle != null)
-                            group = groupEle.text().split("/")[0].toInt()
-
-                        lessons.add(Lesson(
-                            lessonEle.selectFirst(".subject")!!.attr("title"),
-                            lessonEle.selectFirst(".subject")!!.text(),
-                            lessonEle.selectFirst(".employee")!!.attr("title"),
-                            lessonEle.selectFirst(".employee")!!.text(),
-                            lessonEle.selectFirst(".room")!!.text(),
-                            group
-                        ))
-                    }
-
-                    timetablePageBuilder.addLessonSpot(day, LessonSpot(lessons))
-                }
+                    timetablePageBuilder.addLessonSpot(day, parseLessonSpot(lessonSpotEle))
             }
 
             return timetablePageBuilder.build()
@@ -88,5 +56,67 @@ class HtmlTimetableParserImpl : HtmlTimetablePageParser
         {
             throw ParseException(e)
         }
+    }
+
+    /**
+     * Parses [LessonPeriod] from it's HTML element.
+     *
+     * @return The parsed [LessonPeriod].
+     */
+    private fun parseLessonPeriod(lessonPeriodEle: Element) = LessonPeriod.fromString(lessonPeriodEle.selectFirst(".time")!!.text())
+
+    /**
+     * Parses [LessonSpot] from it's HTML element.
+     *
+     * @return The parsed [LessonSpot].
+     */
+    private fun parseLessonSpot(lessonSpotEle: Element): LessonSpot?
+    {
+        /* Skip if the lesson spot is empty, this leaves the lesson variable to null -> indicating no lesson. */
+        if (lessonSpotEle.hasClass("empty"))
+            return null
+
+        /* All the lessons in the lesson spot. */
+        val lessonEles = lessonSpotEle.getElementsByTag("div")
+
+        val lessons = emptyMutableLinkedList<Lesson>()
+
+        for (lessonEle in lessonEles)
+            lessons.add(parseLesson(lessonEle))
+
+        return LessonSpot(lessons)
+    }
+
+    /**
+     * Parses [Lesson] from it's HTML element.
+     *
+     * @return The parsed [Lesson].
+     */
+    private fun parseLesson(lessonEle: Element): Lesson
+    {
+        /* Group is nullable.
+        * Is null when there is only a single lesson in a lesson spot. */
+        val groupEle = lessonEle.selectFirst(".group")
+        val subjectEle = lessonEle.selectFirst(".subject")!!
+        val employeeEle = lessonEle.selectFirst(".employee")!!
+
+        /* Group 0 indicates, that there are no groups, thus whole class has the lesson. */
+        val group = if (groupEle != null)
+            groupEle.text().split("/")[0].toInt()
+        else
+            0
+
+        val subjectName = Name(subjectEle.attr("title"),
+                               subjectEle.text())
+
+        val teacherName = Name(employeeEle.attr("title"),
+                               employeeEle.text())
+
+        return Lesson(
+            subjectName = subjectName,
+            teacherName = teacherName,
+            classroom = lessonEle.selectFirst(".room")!!.text(),
+            group = group
+        )
     }
 }
