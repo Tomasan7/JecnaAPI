c2f55d44e1754ad6507eb2d4a0b62ada4161b055
Tomasan7 <tomashula06@gmail.com>
2022-06-27 20:00:59 +0200

    Make Lesson use Name class

--- src/main/kotlin/me/tomasan7/jecnaapi/data/Lesson.kt
+++ src/main/kotlin/me/tomasan7/jecnaapi/data/Lesson.kt
@@ -1,13 +1,13 @@
 package me.tomasan7.jecnaapi.data
 
+import me.tomasan7.jecnaapi.util.Name
+
 /**
  * A lesson in a timetable.
  *
  * @param group The group's number. Will be {@code 0}, when there aren't groups.
  */
-data class Lesson(val subject: String,
-                  val subjectShort: String,
-                  val teacher: String,
-                  val teacherShort: String,
+data class Lesson(val subjectName: Name,
+                  val teacherName: Name,
                   val classroom: String,
                   val group: Int)
