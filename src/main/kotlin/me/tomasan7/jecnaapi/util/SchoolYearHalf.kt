489d8bec18adcd8bb2137a098299d0030b39cc48
Tomasan7 <tomashula06@gmail.com>
2022-07-31 11:39:53 +0200

    Create static SchoolYearHalf#current() function

--- src/main/kotlin/me/tomasan7/jecnaapi/util/SchoolYearHalf.kt
+++ src/main/kotlin/me/tomasan7/jecnaapi/util/SchoolYearHalf.kt
@@ -1,7 +1,21 @@
 package me.tomasan7.jecnaapi.util
 
+import java.time.LocalDate
+import java.time.Month
+
 enum class SchoolYearHalf
 {
     FIRST,
-    SECOND
+    SECOND;
+
+    companion object
+    {
+        /**
+         * @return Current [SchoolYearHalf].
+         */
+        fun current() = if (LocalDate.now().month !in Month.FEBRUARY..Month.AUGUST)
+            SchoolYearHalf.FIRST
+        else
+            SchoolYearHalf.SECOND
+    }
 }
