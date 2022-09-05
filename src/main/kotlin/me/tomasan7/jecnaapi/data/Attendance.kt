465fc687f3b4fd24acdf3d1a03de4663160172dc
Tomasan7 <tomashula06@gmail.com>
2022-09-05 19:53:12 +0200

    Add an AttendanceType enum

--- src/main/kotlin/me/tomasan7/jecnaapi/data/Attendance.kt
+++ src/main/kotlin/me/tomasan7/jecnaapi/data/Attendance.kt
@@ -7,10 +7,10 @@ import java.time.format.DateTimeFormatter
  * Represents single pass by the school entrance.
  * It can be either enter, or exit.
  *
- * @property exit Whether the person exited or entered. `false` for enter, `true` for exit.
+ * @property type [AttendanceType] about whether the person exited or entered.
  * @property time The time, the person entered/exited.
  */
-class Attendance(val exit: Boolean, val time: LocalTime)
+class Attendance(val type: AttendanceType, val time: LocalTime)
 {
-    override fun toString() = (if (exit) "Odchod" else "Příchod") + " " + time.format(DateTimeFormatter.ofPattern("HH:mm"))
+    override fun toString() = (if (type == AttendanceType.ENTER) "Příchod" else "Odchod") + " " + time.format(DateTimeFormatter.ofPattern("HH:mm"))
 }
---
+++ src/main/kotlin/me/tomasan7/jecnaapi/data/AttendanceType.kt
@@ -0,0 +1,10 @@
+package me.tomasan7.jecnaapi.data
+
+/**
+ * Type of an [Attendance]. Represents either student entering or exiting the school.
+ */
+enum class AttendanceType
+{
+    ENTER,
+    EXIT
+}
