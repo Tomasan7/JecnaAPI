c81c95fb5350ec40d380c038917e6f314a8705f0
Tomasan7 <tomashula06@gmail.com>
2022-09-05 19:53:43 +0200

    Make Attendance a data class and remove it's toString()

--- src/main/kotlin/me/tomasan7/jecnaapi/data/Attendance.kt
+++ src/main/kotlin/me/tomasan7/jecnaapi/data/Attendance.kt
@@ -10,7 +10,4 @@ import java.time.format.DateTimeFormatter
  * @property type [AttendanceType] about whether the person exited or entered.
  * @property time The time, the person entered/exited.
  */
-class Attendance(val type: AttendanceType, val time: LocalTime)
-{
-    override fun toString() = (if (type == AttendanceType.ENTER) "Příchod" else "Odchod") + " " + time.format(DateTimeFormatter.ofPattern("HH:mm"))
-}
+data class Attendance(val type: AttendanceType, val time: LocalTime)
