68149d5ece5b7234db339475817903fa2763931f
Tomasan7 <tomashula06@gmail.com>
2022-09-16 16:36:29 +0200

    Change listOf() to emptyList()

--- src/main/kotlin/me/tomasan7/jecnaapi/data/attendance/AttendancesPage.kt
+++ src/main/kotlin/me/tomasan7/jecnaapi/data/attendance/AttendancesPage.kt
@@ -17,7 +17,7 @@ class AttendancesPage private constructor(private val attendances: Map<LocalDate
     /**
      * @return All [attendances][Attendance] for the provided day. Returns an empty list when no data for [day] is present.
      */
-    operator fun get(day: LocalDate) = attendances.getOrDefault(day, listOf())
+    operator fun get(day: LocalDate) = attendances.getOrDefault(day, emptyList())
 
     class Builder
     {
