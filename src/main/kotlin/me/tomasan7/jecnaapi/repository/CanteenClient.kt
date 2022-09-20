f3bdabee37b674f7c8ddae2cd6906c465cb068d1
Tomasan7 <tomashula06@gmail.com>
2022-09-20 17:28:43 +0100

    Add comments to CanteenClient

--- src/main/kotlin/me/tomasan7/jecnaapi/repository/CanteenClient.kt
+++ src/main/kotlin/me/tomasan7/jecnaapi/repository/CanteenClient.kt
@@ -3,9 +3,17 @@ package me.tomasan7.jecnaapi.repository
 import me.tomasan7.jecnaapi.data.canteen.Menu
 import me.tomasan7.jecnaapi.data.canteen.MenuItem
 
+/**
+ * A client to get and order menus.
+ */
 interface CanteenClient
 {
     suspend fun getMenu(): Menu
 
+    /**
+     * Orders the [menuItem].
+     *
+     * @return Whether the order was successful or not.
+     */
     suspend fun order(menuItem: MenuItem): Boolean
 }
