108a4e97350dbff6acc8c04747d7d35e591ede29
Tomasan7 <tomashula06@gmail.com>
2022-09-20 14:45:46 +0100

    Make GradeAverageCalculators be able to return null

--- src/main/kotlin/me/tomasan7/jecnaapi/data/grade/GradeAverageCalculator.kt
+++ src/main/kotlin/me/tomasan7/jecnaapi/data/grade/GradeAverageCalculator.kt
@@ -10,9 +10,9 @@ interface GradeAverageCalculator
      * Calculates [Grades] average.
      *
      * @param grades The [Grades] to calculate the average from.
-     * @return The calculated average grade as [Float].
+     * @return The calculated average grade as [Float]. Or `null`, if the calculation is not possible. (eg. there are no grades)
      */
-    fun calculate(grades: Grades): Float
+    fun calculate(grades: Grades): Float?
 
     operator fun invoke(grades: Grades) = calculate(grades)
 }
