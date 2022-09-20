3b4f55443df9206d34af496bfbd8d8b98a4fdb07
Tomasan7 <tomashula06@gmail.com>
2022-09-20 17:30:53 +0100

    Fix WebCanteenClient#order(MenuItem) always returning true

--- src/main/kotlin/me/tomasan7/jecnaapi/repository/WebCanteenClient.kt
+++ src/main/kotlin/me/tomasan7/jecnaapi/repository/WebCanteenClient.kt
@@ -1,6 +1,5 @@
 package me.tomasan7.jecnaapi.repository
 
-import io.ktor.http.*
 import me.tomasan7.jecnaapi.data.canteen.MenuItem
 import me.tomasan7.jecnaapi.parser.parsers.HtmlCanteenParser
 import me.tomasan7.jecnaapi.web.ICanteenWebClient
@@ -12,7 +11,13 @@ class WebCanteenClient(
 {
     override suspend fun getMenu() = canteenParser.parse(webClient.queryStringBody(WEB_PATH))
 
-    override suspend fun order(menuItem: MenuItem) = webClient.query("/faces/secured/" + menuItem.orderURL).status == HttpStatusCode.OK
+    override suspend fun order(menuItem: MenuItem): Boolean
+    {
+        val response = webClient.queryStringBody("/faces/secured/" + menuItem.orderURL)
+
+        /* Server responds with HTML, when everything went right or JSON error if something went wrong. */
+        return response.startsWith('<')
+    }
 
     companion object
     {
