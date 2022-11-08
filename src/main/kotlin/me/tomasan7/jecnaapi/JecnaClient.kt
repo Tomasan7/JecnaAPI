027042321eaaa6e58ea10da7adc8bdb3945444d0
Tomasan7 <tomashula06@gmail.com>
2022-11-08 20:19:20 +0100

    Move implementation from repositories to JecnaClient

--- src/main/kotlin/me/tomasan7/jecnaapi/JecnaClient.kt
+++ src/main/kotlin/me/tomasan7/jecnaapi/JecnaClient.kt
@@ -1,49 +1,73 @@
 package me.tomasan7.jecnaapi
 
+import io.ktor.http.*
 import me.tomasan7.jecnaapi.data.timetable.TimetablePage
-import me.tomasan7.jecnaapi.repository.WebAttendancesRepository
-import me.tomasan7.jecnaapi.repository.WebGradesRepository
-import me.tomasan7.jecnaapi.repository.WebNewsRepository
-import me.tomasan7.jecnaapi.repository.WebTimetableRepository
+import me.tomasan7.jecnaapi.parser.parsers.*
+import me.tomasan7.jecnaapi.util.JecnaPeriodEncoder
+import me.tomasan7.jecnaapi.util.JecnaPeriodEncoder.jecnaEncode
 import me.tomasan7.jecnaapi.util.SchoolYear
 import me.tomasan7.jecnaapi.util.SchoolYearHalf
 import me.tomasan7.jecnaapi.web.Auth
 import me.tomasan7.jecnaapi.web.JecnaWebClient
+import me.tomasan7.jecnaapi.web.append
 import java.time.Month
 
 class JecnaClient
 {
-    private val jecnaWebClient = JecnaWebClient()
+    private val webClient = JecnaWebClient()
 
     val lastLoginAuth: Auth?
-        get() = jecnaWebClient.lastLoginAuth
+        get() = webClient.lastLoginAuth
 
-    private val gradesRepository = WebGradesRepository(jecnaWebClient)
-    private val newsRepository = WebNewsRepository(jecnaWebClient)
-    private val timetableRepository = WebTimetableRepository(jecnaWebClient)
-    private val attendancesRepository = WebAttendancesRepository(jecnaWebClient)
+    private val newsPageParser: HtmlNewsPageParser = HtmlNewsPageParserImpl
+    private val gradesPageParser: HtmlGradesPageParser = HtmlGradesPageParserImpl
+    private val timetablePageParser: HtmlTimetablePageParser = HtmlTimetableParserImpl
+    private val attendancesPageParser: HtmlAttendancesPageParser = HtmlAttendancesPageParserImpl
 
     suspend fun login(username: String, password: String) = login(Auth(username, password))
 
-    suspend fun login(auth: Auth) = jecnaWebClient.login(auth)
+    suspend fun login(auth: Auth) = webClient.login(auth)
 
-    suspend fun logout() = jecnaWebClient.logout()
+    suspend fun logout() = webClient.logout()
 
-    suspend fun isLoggedIn() = jecnaWebClient.isLoggedIn()
+    suspend fun isLoggedIn() = webClient.isLoggedIn()
 
-    suspend fun getGradesPage(schoolYear: SchoolYear, schoolYearHalf: SchoolYearHalf) = gradesRepository.queryGradesPage(schoolYear, schoolYearHalf)
+    suspend fun getNewsPage() = newsPageParser.parse(webClient.queryStringBody(PageWebPath.news))
 
-    suspend fun getGradesPage() = gradesRepository.queryGradesPage()
+    suspend fun getGradesPage(schoolYear: SchoolYear, schoolYearHalf: SchoolYearHalf) =
+        gradesPageParser.parse(webClient.queryStringBody(PageWebPath.grades, Parameters.build {
+            append(schoolYear.jecnaEncode())
+            append(schoolYearHalf.jecnaEncode())
+        }))
 
-    suspend fun getNewsPage() = newsRepository.queryNewsPage()
+    suspend fun getGradesPage() = gradesPageParser.parse(webClient.queryStringBody(PageWebPath.grades))
 
-    suspend fun getTimetablePage(schoolYear: SchoolYear, periodOption: TimetablePage.PeriodOption? = null) = timetableRepository.queryTimetablePage(schoolYear, periodOption)
+    suspend fun getTimetablePage(schoolYear: SchoolYear, periodOption: TimetablePage.PeriodOption? = null) =
+        timetablePageParser.parse(webClient.queryStringBody(PageWebPath.timetable, Parameters.build {
+            append(schoolYear.jecnaEncode())
+            periodOption?.let { append(it.jecnaEncode()) }
+        }))
 
-    suspend fun getTimetablePage() = timetableRepository.queryTimetablePage()
+    suspend fun getTimetablePage() = timetablePageParser.parse(webClient.queryStringBody(PageWebPath.timetable))
 
-    suspend fun getAttendancesPage(schoolYear: SchoolYear, month: Month) = attendancesRepository.queryAttendancesPage(schoolYear, month)
+    suspend fun getAttendancesPage(schoolYear: SchoolYear, month: Month) = getAttendancesPage(schoolYear, month.value)
 
-    suspend fun getAttendancesPage(schoolYear: SchoolYear, monthId: Int) = attendancesRepository.queryAttendancesPage(schoolYear, monthId)
+    suspend fun getAttendancesPage(schoolYear: SchoolYear, month: Int) =
+        attendancesPageParser.parse(webClient.queryStringBody(PageWebPath.attendances, Parameters.build {
+            append(schoolYear.jecnaEncode())
+            append(JecnaPeriodEncoder.encodeMonth(month))
+        }))
 
-    suspend fun getAttendancesPage() = attendancesRepository.queryAttendancesPage()
+    suspend fun getAttendancesPage() = attendancesPageParser.parse(webClient.queryStringBody(PageWebPath.attendances))
+
+    companion object
+    {
+        private object PageWebPath
+        {
+            const val grades = "/score/student"
+            const val news = "/"
+            const val attendances = "/absence/passing-student"
+            const val timetable = "/timetable/class"
+        }
+    }
 }
