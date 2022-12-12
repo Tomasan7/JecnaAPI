68369043191529549a01c48173a8109d7f11803c
Tomasan7 <tomashula06@gmail.com>
2022-12-12 15:30:18 +0100

    Add GradesExample

---
+++ src/main/kotlin/examples/GradesExample.kt
@@ -0,0 +1,31 @@
+package examples
+
+import kotlinx.coroutines.runBlocking
+import me.tomasan7.jecnaapi.JecnaClient
+import me.tomasan7.jecnaapi.util.SchoolYear
+import me.tomasan7.jecnaapi.util.SchoolYearHalf
+
+fun main(): Unit = runBlocking {
+
+    val client = JecnaClient()
+
+    client.login("hula", "password")
+
+    /* Stáhne známky z roku 2021/2022 z druhého pololetí. */
+    val gradesPage = client.getGradesPage(SchoolYear(2021), SchoolYearHalf.SECOND)
+
+    val mathSubject = gradesPage["Matematika"]!!
+
+    val mathAverage = mathSubject.grades.average()
+
+    println("Průměr z matematiky: $mathAverage")
+
+    /* Do grades[] patří string, který určuje část předmětu. (např. Teorie, Cvičení)
+    * Pokud předmět není rozdělen, použijte null. */
+    val mathGrades = mathSubject.grades[null]!!
+
+    println("Známky z matematiky: ")
+
+    for (grade in mathGrades)
+        println(grade.valueChar() + " - " + grade.description)
+}
