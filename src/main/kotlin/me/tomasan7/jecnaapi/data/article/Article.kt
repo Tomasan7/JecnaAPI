d07f8d204ac3220a2b9b0cbe11ab8a3c220b99c2
Tomasan7 <tomashula06@gmail.com>
2023-01-15 19:17:20 +0100

    Make article classes serializable

--- src/main/kotlin/me/tomasan7/jecnaapi/data/article/Article.kt
+++ src/main/kotlin/me/tomasan7/jecnaapi/data/article/Article.kt
@@ -1,14 +1,18 @@
 package me.tomasan7.jecnaapi.data.article
 
+import kotlinx.serialization.Serializable
+import me.tomasan7.jecnaapi.serialization.LocalDateSerializer
 import java.time.LocalDate
 
 /**
  * An article on the main news page.
  */
+@Serializable
 data class Article(
     val title: String,
     val content: String,
     val htmlContent: String,
+    @Serializable(with = LocalDateSerializer::class)
     val date: LocalDate,
     val author: String,
     val schoolOnly: Boolean,
