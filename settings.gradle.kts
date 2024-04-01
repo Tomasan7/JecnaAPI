15d1aac14fcde982e696b09725c2904a05b8efdd
Tomasan7 <tomashula06@gmail.com>
2024-04-01 14:54:06 +0200

    Add foojay-resolver-convention gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,10 @@
 rootProject.name = "jecnaapi"
 include("jecnaapi-java")
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
